1d1aeada3d48e4ce71abd73f3347ba50eda86225
Vicente Penades <megaz0ne23@hotmail.com>
2018-02-27 16:17:28 +0100

    UnitTest more refactoring

--- Sources/Epsylon.TextureSquish.UnitTests/ConversionTests.cs
+++ Sources/Epsylon.TextureSquish.UnitTests/ConversionTests.cs
@@ -36,22 +36,22 @@ namespace Epsylon.TextureSquish.UnitTests
 
         private void _ConversionTest(string method)
         {
-            SquishUtils.ProcessFile(method, "TestFiles\\fight_of_thrones_by_orkimides-d6sa500.png", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\fight_of_thrones_by_orkimides-d6sa500.png",txt=> TestContext.WriteLine(txt));
 
-            SquishUtils.ProcessFile(method, "TestFiles\\squish_test_original.png", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\squish_test_original.png", txt => TestContext.WriteLine(txt));
 
-            SquishUtils.ProcessFile(method, "TestFiles\\UVGrid1.jpg", TestContext);
-            SquishUtils.ProcessFile(method, "TestFiles\\UVGrid2.jpg", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\UVGrid1.jpg", txt => TestContext.WriteLine(txt));
+            SquishUtils.ProcessFile(method, "TestFiles\\UVGrid2.jpg", txt => TestContext.WriteLine(txt));
 
-            SquishUtils.ProcessFile(method, "TestFiles\\Ivy1.png", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\Ivy1.png", txt => TestContext.WriteLine(txt));
 
-            SquishUtils.ProcessFile(method, "TestFiles\\Rainbow_to_alpha_gradient_large.png", TestContext);
-            SquishUtils.ProcessFile(method, "TestFiles\\Rainbow_to_alpha_gradient_small.png", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\Rainbow_to_alpha_gradient_large.png", txt => TestContext.WriteLine(txt));
+            SquishUtils.ProcessFile(method, "TestFiles\\Rainbow_to_alpha_gradient_small.png", txt => TestContext.WriteLine(txt));
 
-            SquishUtils.ProcessFile(method, "TestFiles\\tree.png", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\tree.png", txt => TestContext.WriteLine(txt));
 
-            SquishUtils.ProcessFile(method, "TestFiles\\rocks-diffuse.png", TestContext);
-            SquishUtils.ProcessFile(method, "TestFiles\\rocks-normals.png", TestContext);
+            SquishUtils.ProcessFile(method, "TestFiles\\rocks-diffuse.png", txt => TestContext.WriteLine(txt));
+            SquishUtils.ProcessFile(method, "TestFiles\\rocks-normals.png", txt => TestContext.WriteLine(txt));
         }
 
     }
